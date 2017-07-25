b79c48fe1ed608bb33351dbf7e2f25fe48efbd9f
RJ Madsen <rjm6823@rit.edu>
2017-07-25 11:35:51 -0400

    Updating angular example

--- restaurant/app.js
+++ restaurant/app.js
@@ -11,3 +11,12 @@ angular.module('myApp', [
     $locationProvider.hashPrefix('!');
     $routeProvider.otherwise({redirectTo: '/home'});
 }]);
+
+// Return to top function
+$(window).scroll(function () {
+    if ($(window).scrollTop() + $(window).height() === $(document).height()) {
+        $('.footer').css({"opacity": 1});
+    } else {
+        $('.footer').css({"opacity": 0.75});
+    }
+});
