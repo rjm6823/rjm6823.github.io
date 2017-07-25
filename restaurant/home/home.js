2b84ecc61eb04691274dbdbc272913e869c5b69a
RJ Madsen <rjm6823@rit.edu>
2017-07-25 17:45:17 -0400

    Angular example updates

--- restaurant/home/home.js
+++ restaurant/home/home.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.home', ['ngRoute'])
+angular.module('myApp.home', ['ngRoute','ngAnimate', 'ngSanitize', 'ui.bootstrap'])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/home', {
@@ -9,6 +9,26 @@ angular.module('myApp.home', ['ngRoute'])
         });
     }])
 
-    .controller('HomeCtrl', [function () {
+    .controller('HomeCtrl', ['$scope', '$http', function ($scope, $http) {
+        $scope.myInterval = 5000;
+        $scope.noWrapSlides = false;
+        $scope.active = 0;
 
+         // Fetching data from JSON files
+        $http.get('data/breakfast.json')
+            .then(function (data) {
+                $scope.breakfast = data.data.breakfastArray;
+            });
+        $http.get('data/lunch.json')
+            .then(function (data) {
+                $scope.lunch = data.data.lunchArray;
+            });
+        $http.get('data/dinner.json')
+            .then(function (data) {
+                $scope.dinner = data.data.dinnerArray;
+            });
+        $http.get('data/events.json')
+            .then(function (data) {
+                $scope.events = data.data.eventsArray;
+            });
     }]);
